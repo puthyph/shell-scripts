c62b674aa86653c7af5dc1aba10cd4425a0e9def
puthyph <59962565+puthyph@users.noreply.github.com>
2024-07-15 21:22:02 +0200

    refactor(mount-disk.sh): Updating script to dynamically create partitions from DISKS_LIST array

--- mount-disk.sh
+++ mount-disk.sh
@@ -1,27 +1,64 @@
 #!/bin/bash
 
-DISK="/dev/sdb"
-MOUNT_POINT="/applis"
+# Initializing arrays
+declare -A DISKS_LIST
+DISKS_LIST["DOCKER_DISK"]="/dev/sdc;/applis"
+DISKS_LIST["MOUNT_POINT"]="/dev/sdd;/var/lib/docker"
 FS_TYPE="ext4"
 
-if [ ! -b "$DISK" ]; then
-    echo "Erreur : Le disque $DISK n'existe pas."
-    exit 1
-fi
+# Reading arrays by converting them into lists
+read_arrays() {
+  for key in "${!DISKS_LIST[@]}"; do
+    local elements=${DISKS_LIST[$key]}
+    while IFS=';' read -r disk mountpoint
+    do
+        echo "disk=$disk mountpoint=$mountpoint"
+        check_disk
+        check_mountpoint
+	create_partition
+    done <<< "$elements"
 
-if [ ! -d "$MOUNT_POINT" ]; then
-    sudo mkdir -p "$MOUNT_POINT"
-fi
+  done
+}
 
-sudo parted -s "$DISK" mklabel gpt
-sudo parted -s -a optimal "$DISK" mkpart primary 0% 100%
+check_disk() {
+    if [ ! -b "$disk" ]; then
+        echo "Error : Disk does not exist."
+        exit 1
+    fi
+}
 
-PARTITION="${DISK}1"
-sudo mkfs -t "$FS_TYPE" "$PARTITION"
+check_mountpoint() {
+    if [ ! -b "$mountpoint" ]; then
+	echo "Mountpoint doesn't exist, creating the directory $mountpoint."
+	echo "sudo mkdir -p $mountpoint"
+        sudo mkdir -p $mountpoint
+    fi
+}
 
-sudo mount "$PARTITION" "$MOUNT_POINT"
+create_partition() {
+    # Creating partition label
+    echo "parted -s $disk mklabel gpt"
+    sudo parted -s $disk mklabel gpt
 
-UUID=$(sudo blkid -s UUID -o value "$PARTITION")
-echo "UUID=$UUID $MOUNT_POINT $FS_TYPE defaults 0 2" | sudo tee -a /etc/fstab
+    # Creating partition
+    echo "sudo parted -s -a optimal $disk mkpart primary 0% 100%"
+    sudo parted -s -a optimal "$disk" mkpart primary 0% 100%
 
-echo "Le disque $DISK est monté sur $MOUNT_POINT."
+    PARTITION="${disk}1"
+
+    # Creating filesystem from partition
+    echo "sudo mkfs -t $FS_TYPE $PARTITION"
+    sudo mkfs -t $FS_TYPE $PARTITION
+
+    # Mounting partition on mountpoint
+    echo "sudo mount $PARTITION $mountpoint"
+    sudo mount $PARTITION $mountpoint
+
+    # Retrieving UUID to put it in /etc/fstab
+    UUID=$(sudo blkid -s UUID -o value "$PARTITION")
+    echo "UUID=$UUID $disk $FS_TYPE defaults 0 2" | sudo tee -a /etc/fstab
+    echo "Disk $disk mounted on $mountpoint."
+}
+
+read_arrays
